25b2293aaf0a604a4b95c2b8cf9a4c3c305df1fc
John-Hanekom <113033854+John-Hanekom@users.noreply.github.com>
2022-12-15 09:13:27 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let distance = 0
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.pause(1000)
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distance)
+    basic.pause(100)
+})
